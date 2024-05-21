d5992d5ea48eddb5a6cf93c1f0eb9fb5380c9fa3
nichellesequeira <nichelle.sequeira@epfl.ch>
2024-05-21 13:48:18 +0200

    Initial Commit

---
+++ src/protavision/__init__.py
@@ -0,0 +1,5 @@
+"""This project is part of the Practical Programming in Chemistry course given at EPFL (spring semester 2024), with aim to familiriaze ourselves with using GitHub and collaborating on a programming project together.."""
+
+from __future__ import annotations
+
+__version__ = "0.0.1"
